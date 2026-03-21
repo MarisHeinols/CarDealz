e5a0fd1ba55817234427dbe1270b2f531096d651
marisHeinols <maris@rucapenlabs.com>
2026-03-21 21:19:23 +0200

    Fix github actions missing firebase env variables

--- app/firebase/fireBaseConfig.ts
+++ app/firebase/fireBaseConfig.ts
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "mock-api-key",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "mock-domain",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "mock-project",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "mock-bucket",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "mock-sender",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "mock-app-id",
 };
 
 export const app = initializeApp(firebaseConfig);
