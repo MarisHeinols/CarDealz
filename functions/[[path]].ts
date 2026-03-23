d65c513d120692f0757605381702526dc3ef0d48
marisHeinols <maris@rucapenlabs.com>
2026-03-23 19:22:45 +0200

    Fixed site map and routing

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,6 +1,17 @@
 import { createPagesFunctionHandler } from "@react-router/cloudflare";
+import type { PagesFunction } from "@cloudflare/workers-types";
 
 // @ts-ignore - The build output won't exist until you run `npm run build`
 import * as build from "../build/server/index.js";
 
-export const onRequest = createPagesFunctionHandler({ build });
+const handler = createPagesFunctionHandler({ build });
+
+export const onRequest: PagesFunction = async (context) => {
+  const url = new URL(context.request.url);
+  console.log("[pages]", context.request.method, url.pathname);
+
+  const response = await handler(context);
+
+  console.log("[pages]", context.request.method, url.pathname, "->", response.status);
+  return response;
+};
